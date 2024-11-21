2b2f758ee1e87934de8737f7c8e06397502cf65b
bbel <bweb33@protonmail.com>
2024-11-21 15:18:13 +0100

    exo2

--- utils/db.mjs
+++ utils/db.mjs
@@ -6,37 +6,75 @@ class DBClient {
     this.host = process.env.DB_HOST || 'localhost';
     this.port = process.env.DB_PORT || '27017';
     this.database = process.env.DB_DATABASE || 'files_manager';
-
     this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true });
   }
 
-  isAlive() {
-    try {
-      this.client.connect();
-      this.client.db(this.database).command({ ping: 1 });
-      return true;
-    } catch (error) {
-      console.log(`Error connecting to MongoDB: ${error}`);
-      return false;
-    }
+  isAlive(callback) {
+    this.client.connect((err) => {
+      if (err) {
+        console.log(`Error connecting to MongoDB: ${err}`);
+        callback(false);
+      } else {
+        this.client.db(this.database).command({ ping: 1 }, (err, result) => {
+          if (err) {
+            console.log(`Error pinging MongoDB: ${err}`);
+            callback(false);
+          } else {
+            callback(true);
+          }
+        });
+      }
+    });
   }
 
-  async nbUsers() {
-    await this.client.connect();
-    const db = this.client.db(this.database);
-    const usersCollection = db.collection('users');
-    const userCount = await usersCollection.countDocuments();
-    await this.client.close();
-    return userCount;
+  nbUsers(callback) {
+    this.client.connect((err) => {
+      if (err) {
+        console.log(`Error connecting to MongoDB: ${err}`);
+        callback(null);
+      } else {
+        const db = this.client.db(this.database);
+        const usersCollection = db.collection('users');
+        usersCollection.countDocuments((err, count) => {
+          if (err) {
+            console.log(`Error counting users: ${err}`);
+            callback(null);
+          } else {
+            this.client.close((err) => {
+              if (err) {
+                console.log(`Error closing MongoDB connection: ${err}`);
+              }
+              callback(count);
+            });
+          }
+        });
+      }
+    });
   }
 
-  async nbFiles() {
-    await this.client.connect();
-    const db = this.client.db(this.database);
-    const usersCollection = db.collection('files');
-    const fileCount = await usersCollection.countDocuments();
-    await this.client.close();
-    return fileCount;
+  nbFiles(callback) {
+    this.client.connect((err) => {
+      if (err) {
+        console.log(`Error connecting to MongoDB: ${err}`);
+        callback(null);
+      } else {
+        const db = this.client.db(this.database);
+        const usersCollection = db.collection('files');
+        usersCollection.countDocuments((err, count) => {
+          if (err) {
+            console.log(`Error counting files: ${err}`);
+            callback(null);
+          } else {
+            this.client.close((err) => {
+              if (err) {
+                console.log(`Error closing MongoDB connection: ${err}`);
+              }
+              callback(count);
+            });
+          }
+        });
+      }
+    });
   }
 }
 
